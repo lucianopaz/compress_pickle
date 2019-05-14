5ff7c79e8305b82ecb824fe1e777309e8e8db7e4
lucianopaz <luciano.paz.neuro@gmail.com>
2019-05-14 12:09:11 +0200

    Moved tests to bash script

---
+++ scripts/run_tests.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -ex # fail on first error, print commands
+
+if [ ! -z ${TEST_STYLE} ]
+then
+    @printf "Checking code style with black...\n";
+    black --check compress_pickle;
+    @printf "\033[1;34mBlack passes!\033[0m\n\n";
+    @printf "Checking code style with pylint...\n";
+    pylint compress_pickle;
+    @printf "\033[1;34mPylint passes!\033[0m\n\n";
+fi
+
+pytest -v compress_pickle/tests/ --cov=compress_pickle/ --html=testing-report.html --self-contained-html
