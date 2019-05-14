22ef9d5dd1dfc1c14dc550d95111d241165e528a
lucianopaz <luciano.paz.neuro@gmail.com>
2019-05-14 15:21:34 +0200

    Simplified classifiers

--- setup.py
+++ setup.py
@@ -52,9 +52,6 @@ if __name__ == "__main__":
         include_package_data=True,
         classifiers=[
             "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.5"
-            "Programming Language :: Python :: 3.6"
-            "Programming Language :: Python :: 3.7"
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
         ],
