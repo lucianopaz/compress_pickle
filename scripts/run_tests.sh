073685c5ffc0ca560116dc8d1e38f920a0340fc0
lucianopaz <luciano.paz.neuro@gmail.com>
2019-05-14 12:14:24 +0200

    Fixed tests script

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,12 +4,12 @@ set -ex # fail on first error, print commands
 
 if [ ! -z ${TEST_STYLE} ]
 then
-    @printf "Checking code style with black...\n";
+    printf "Checking code style with black...\n";
     black --check compress_pickle;
-    @printf "\033[1;34mBlack passes!\033[0m\n\n";
-    @printf "Checking code style with pylint...\n";
+    printf "\033[1;34mBlack passes!\033[0m\n\n";
+    printf "Checking code style with pylint...\n";
     pylint compress_pickle;
-    @printf "\033[1;34mPylint passes!\033[0m\n\n";
+    printf "\033[1;34mPylint passes!\033[0m\n\n";
 fi
 
 pytest -v compress_pickle/tests/ --cov=compress_pickle/ --html=testing-report.html --self-contained-html
