0cd9195124c8c10472ccbae957afcb5b23d53358
lucianopaz <luciano.paz.neuro@gmail.com>
2019-09-07 10:59:54 +0200

    Trying to fix if

--- scripts/create_testenv.sh
+++ scripts/create_testenv.sh
@@ -36,7 +36,18 @@ fi
 pip install --upgrade pip
 pip install -r requirements.txt
 
-if [ ! -z ${TEST_STYLE} ] || [ ! -z ${TEST_DOCS} ]
+if [ ! -z ${TEST_STYLE} ]
+then
+    DEVREQS="true"
+fi
+
+if [ ! -z ${TEST_DOCS} ]
+then
+    DEVREQS="true"
+fi
+
+
+if [ ! -z ${DEVREQS} ]
 then
     pip install -r requirements-dev.txt
 else
