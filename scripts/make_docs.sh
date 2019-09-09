23278ef43ad0b55475da8cec985fd1f83f12b8d8
lucianopaz <luciano.paz.neuro@gmail.com>
2019-09-09 14:22:01 +0200

    Added printf statements to shell scripts

--- scripts/make_docs.sh
+++ scripts/make_docs.sh
@@ -4,4 +4,6 @@ set -ex # fail on first error, print commands
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "${DIR}/../docs"
+printf "Making html docs\n";
 make html
+printf "\033[1;34mDocumentation html correctly built!\033[0m\n\n";
