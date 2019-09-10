c2119758184699250f3bd111dfa76a6b4a701b72
Luciano Paz <chulo1787@gmail.com>
2019-09-10 15:00:37 +0200

    Making compatible with pipelines method

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,7 +2,7 @@
 
 set -ex # fail on first error, print commands
 
-if [ ! -z ${TEST_STYLE} ]
+if [ ! -z ${TEST_STYLE} ] && [ ${TEST_STYLE} = "True" ]
 then
     printf "Checking code style with black...\n";
     black --check compress_pickle;
@@ -12,12 +12,12 @@ then
     printf "\033[1;34mPylint passes!\033[0m\n\n";
 fi
 
-if [ ! -z ${TEST_DOCS} ]
+if [ ! -z ${TEST_DOCS} ] && [ ${TEST_DOCS} = "True" ]
 then
     sphinx-build -nWT docs/source docs/html;
 fi
 
-if [ ! -z ${TEST_UNITTESTS} ]
+if [ ! -z ${TEST_UNITTESTS} ] && [ ${TEST_UNITTESTS} = "True" ]
 then
     pytest -v compress_pickle/tests/ --cov=compress_pickle/ --html=testing-report.html --self-contained-html;
 fi
