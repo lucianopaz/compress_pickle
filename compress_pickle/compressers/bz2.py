2e2a02ba87f479834e468f63cab442e09c4c0daa
lucianopaz <luciano.paz.neuro@gmail.com>
2021-03-03 15:03:16 +0100

    Add bz2 compresser

---
+++ compress_pickle/compressers/bz2.py
@@ -0,0 +1,27 @@
+from io import IOBase
+from typing import Union
+import bz2
+from .base import BaseCompresser, PathType, PATH_TYPES
+from .registry import register_compresser
+
+
+class Bz2Compresser(BaseCompresser):
+    def __init__(self, path: Union[PathType, IOBase], mode: str, **kwargs):
+        if not isinstance(PATH_TYPES + (IOBase,)):
+            raise TypeError(f"Unhandled path type {type(path)}")
+        self._stream = bz2.open(file=path, mode=mode, **kwargs)
+
+    def close(self):
+        self._stream.close()
+
+    def get_stream(self) -> IOBase:
+        return self._stream
+
+
+register_compresser(
+    name="bz2",
+    compresser=Bz2Compresser,
+    extensions=["bz", "bz2"],
+    default_write_mode="wb",
+    default_read_mode="rb",
+)
