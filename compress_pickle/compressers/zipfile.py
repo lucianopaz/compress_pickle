fd0d48ed82429dcbe2960bb75bff549e7fc539cb
lucianopaz <luciano.paz.neuro@gmail.com>
2021-03-03 15:03:39 +0100

    Add zipfile compresser

---
+++ compress_pickle/compressers/zipfile.py
@@ -0,0 +1,40 @@
+from os.path import basename
+from io import IOBase
+from typing import Union
+import zipfile
+from .base import BaseCompresser, PathType, PATH_TYPES
+from .registry import register_compresser
+
+
+class ZipfileCompresser(BaseCompresser):
+    def __init__(
+        self, path: Union[PathType, IOBase], mode: str, arcname=None, pwd=None, **kwargs
+    ):
+        if not isinstance(PATH_TYPES + (IOBase,)):
+            raise TypeError(f"Unhandled path type {type(path)}")
+        self._arch = zipfile.ZipFile(path, mode=mode, **kwargs)
+        if arcname is None:
+            if isinstance(path, PATH_TYPES):
+                file_path = basename(path)
+            else:
+                file_path = getattr(path, "name", "default")
+            arcname = file_path
+        else:
+            file_path = arcname
+        self._stream = self._arch.open(file_path, mode=mode, pwd=pwd)
+
+    def close(self):
+        self._stream.close()
+        self._arch.close()
+
+    def get_stream(self) -> IOBase:
+        return self._stream
+
+
+register_compresser(
+    name="zipfile",
+    compresser=ZipfileCompresser,
+    extensions=["zip"],
+    default_write_mode="w",
+    default_read_mode="r",
+)
