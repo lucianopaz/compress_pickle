fb55da21c16d57fd5912bf0d4ff1120d7a0373af
lucianopaz <luciano.paz.neuro@gmail.com>
2021-09-21 19:26:38 +0200

    Document how to see registerred compressers and picklers

--- compress_pickle/__init__.py
+++ compress_pickle/__init__.py
@@ -1,7 +1,10 @@
-from .compress_pickle import dump, load, dumps, loads
-from . import utils
-from . import compressers
-from . import picklers
-from . import io
+from . import compressers, io, picklers, utils
+from .compress_pickle import dump, dumps, load, loads
+from .compressers.registry import (
+    get_default_compression_mapping,
+    get_known_compressions,
+    get_registered_extensions,
+)
+from .picklers.registry import get_known_picklers
 
 __version__ = "2.0.1"
